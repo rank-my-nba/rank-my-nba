350f77e5585f8f0469d5243c479acd0bd1450cc9
Robert Hoover <peelintaters85@gmail.com>
2023-11-25 11:54:01 -0700

    Can get data from API

--- server/server.js
+++ server/server.js
@@ -1,12 +1,12 @@
 const path = require('path');
-const cors = require('cors');
+
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const PORT = 3000;
 
 const app = express();
 
-app.use(cors({ credentials: true, origin: true }));
+// app.use(cors({ credentials: true, origin: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
@@ -14,6 +14,19 @@ app.use(express.static(path.join(__dirname, '/build')));
 
 app.get('/', (req, res) => {
   return res.status(200).sendFile(path.join(__dirname, '../src/index.html'));
+});
+
+app.get('/api/nba', async (req, res) => {
+  try {
+    const response = await fetch(
+      `https://api.sportradar.us/nba/trial/v8/en/seasons/2022/REG/leaders.json?api_key=2s3pjdfykafdp3ztfqbka7bh`
+    );
+    const data = await response.json();
+    console.log(data);
+    res.status(200).send(data);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 // need catch all route and global error handler
