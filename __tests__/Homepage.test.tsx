5834af6964e592a397f3458ec6801b0a86bb7d0a
Robert Hoover <peelintaters85@gmail.com>
2023-11-28 19:30:18 -0700

    Testing works now

--- __tests__/Homepage.test.tsx
+++ __tests__/Homepage.test.tsx
@@ -4,10 +4,18 @@ import { cleanup, render, screen } from '@testing-library/react';
 import Homepage from '../src/components/Homepage';
 
 describe('Home page', () => {
-  it('renders an h1', () => {
+  beforeEach(() => {
     render(<Homepage />);
+  });
+
+  it('renders an h1', () => {
     const h1Element = screen.getByText('2022-23 NBA Stats');
     expect(h1Element).toBeInTheDocument();
+  });
+
+  it('should render hello world', () => {
+    const div = screen.getByText('Hello world');
+    expect(div).toBeInTheDocument();
   });
 
   afterAll(cleanup);
