581b7187a314eab807ef7c689f256b7fc6760ed1
Robert Hoover <peelintaters85@gmail.com>
2023-11-28 19:23:18 -0700

    Testing works properly now

--- __tests__/Homepage.test.tsx
+++ __tests__/Homepage.test.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Homepage from 'src/components/Homepage';
+import * as React from 'react';
+import '@testing-library/jest-dom';
+import { cleanup, render, screen } from '@testing-library/react';
+import Homepage from '../src/components/Homepage';
 
-it('renders 2022-23 NBA Stats h1', () => {
+describe('Home page', () => {
+  it('renders an h1', () => {
     render(<Homepage />);
     const h1Element = screen.getByText('2022-23 NBA Stats');
-})
+    expect(h1Element).toBeInTheDocument();
+  });
+
+  afterAll(cleanup);
+});
