d597be490f4d1d56c5af7c4d9b7caa0b4b203667
Robert Hoover <peelintaters85@gmail.com>
2023-12-02 12:25:29 -0700

    Fixed the test file for Homepage component. Adds a test for the link component.

--- __tests__/Homepage.test.tsx
+++ __tests__/Homepage.test.tsx
@@ -1,11 +1,21 @@
 import * as React from 'react';
 import '@testing-library/jest-dom';
-import { cleanup, render, screen } from '@testing-library/react';
+import { cleanup, render, screen, waitFor } from '@testing-library/react';
+import { MemoryRouter, Route, Routes } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 import Homepage from '../src/components/Homepage';
+import AuthenticationPage from '../src/components/Login';
 
 describe('Home page', () => {
   beforeEach(() => {
-    render(<Homepage />);
+    render(
+      <MemoryRouter initialEntries={['/']}>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/login" element={<AuthenticationPage />} />
+        </Routes>
+      </MemoryRouter>
+    );
   });
 
   it('renders an h1', () => {
@@ -13,9 +23,12 @@ describe('Home page', () => {
     expect(h1Element).toBeInTheDocument();
   });
 
-  it('should render hello world', () => {
-    const div = screen.getByText('Hello world');
-    expect(div).toBeInTheDocument();
+  it('should navigate to the login page', async () => {
+    userEvent.click(screen.getByRole('link', { name: 'Go to Login' }));
+
+    await waitFor(() => {
+      expect(screen.getByRole('heading', { name: 'Create an account' })).toBeInTheDocument();
+    });
   });
 
   afterAll(cleanup);
